bbcea165e214fc77df55a7d0cd7c33ddbd9702b4
DanOrm94 <142906041+DanOrm94@users.noreply.github.com>
2026-09-09 15:01:52 +0100

    Remove obsolete Astro Tailwind integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
-import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   output: 'server',
   adapter: cloudflare(),
-  integrations: [tailwind()],
   site: 'https://transferchickclothing.pages.dev',
 });
